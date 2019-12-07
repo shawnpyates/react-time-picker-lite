c6871a11d580d069073e48682469e2b6137ae673
Shawn Yates <yates.shawnp@gmail.com>
2019-12-07 10:46:24 -0800

    fixes styled components

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -1,25 +1,30 @@
 import styled from 'styled-components';
 
 const TimePickerWrapper = styled.div`
-  height: ${props.height};
-  width: ${props.width};
+  ${props => `
+    height: ${props.height};
+    width: ${props.width};
+  `}
 `;
 
 const TimePickerInput = styled.input`
-  height: ${props.height};
-  width: ${props.width};
-  background-color: ${props.backgroundColorOnBlur};
-  color: ${props.textColorOnBlur};
   border-radius: 3px;
   border: 1px solid #CCC;
   text-align: center;
   font-weight: 400;
   cursor: pointer;
 
-  &:focus {
-    background-color: ${props.backgroundColorOnFocus};
-    color: ${props.textColorOnFocus};
-  }
+  ${props => `
+    height: ${props.height};
+    width: ${props.width};
+    background-color: ${props.backgroundColorOnFocus}
+    color: ${props.textColorOnBlur}
+
+    &:focus {
+      background-color: ${props.backgroundColorOnFocus}
+      color: ${props.textColorOnFocus}
+    }
+  `}
 `;
 
 module.exports = {
